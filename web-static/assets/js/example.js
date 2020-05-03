eea5ff90117a374b0cb1b53359a2877a492ab008
Josh Fourie <josh.fourie97@gmail.com>
2020-05-03 13:08:00 +1000

    Reverting graph to earlier commit
    Signed-off-by: Josh Fourie <josh.fourie97@gmail.com>

--- web-static/assets/js/example.js
+++ web-static/assets/js/example.js
@@ -4,91 +4,122 @@ var width = svg.attr("width");
 var height = svg.attr("height");
 
 
+/************* Data **************/
+//Characters
+var nodes_data =  [
+    {"name": "Lillian", "sex": "F"},
+    {"name": "Gordon", "sex": "M"},
+    {"name": "Sylvester", "sex": "M"},
+    {"name": "Mary", "sex": "F"},
+    {"name": "Helen", "sex": "F"},
+    {"name": "Jamie", "sex": "M"},
+    {"name": "Jessie", "sex": "F"},
+    {"name": "Ashton", "sex": "M"},
+    {"name": "Duncan", "sex": "M"},
+    {"name": "Evette", "sex": "F"},
+    {"name": "Mauer", "sex": "M"},
+    {"name": "Fray", "sex": "F"},
+    {"name": "Duke", "sex": "M"},
+    {"name": "Baron", "sex": "M"},
+    {"name": "Infante", "sex": "M"},
+    {"name": "Percy", "sex": "M"},
+    {"name": "Cynthia", "sex": "F"}
+]
+
+//Relationships
+//type: A for Ally, E for Enemy
+var links_data = [
+    {"source": "Sylvester", "target": "Gordon", "type":"A" },
+    {"source": "Sylvester", "target": "Lillian", "type":"A" },
+    {"source": "Sylvester", "target": "Mary", "type":"A"},
+    {"source": "Sylvester", "target": "Jamie", "type":"A"},
+    {"source": "Sylvester", "target": "Jessie", "type":"A"},
+    {"source": "Sylvester", "target": "Helen", "type":"A"},
+    {"source": "Helen", "target": "Gordon", "type":"A"},
+    {"source": "Mary", "target": "Lillian", "type":"A"},
+    {"source": "Ashton", "target": "Mary", "type":"A"},
+    {"source": "Duncan", "target": "Jamie", "type":"A"},
+    {"source": "Gordon", "target": "Jessie", "type":"A"},
+    {"source": "Sylvester", "target": "Fray", "type":"E"},
+    {"source": "Fray", "target": "Mauer", "type":"A"},
+    {"source": "Fray", "target": "Cynthia", "type":"A"},
+    {"source": "Fray", "target": "Percy", "type":"A"},
+    {"source": "Percy", "target": "Cynthia", "type":"A"},
+    {"source": "Infante", "target": "Duke", "type":"A"},
+    {"source": "Duke", "target": "Gordon", "type":"A"},
+    {"source": "Duke", "target": "Sylvester", "type":"A"},
+    {"source": "Baron", "target": "Duke", "type":"A"},
+    {"source": "Baron", "target": "Sylvester", "type":"E"},
+    {"source": "Evette", "target": "Sylvester", "type":"E"},
+    {"source": "Cynthia", "target": "Sylvester", "type":"E"},
+    {"source": "Cynthia", "target": "Jamie", "type":"E"},
+    {"source": "Mauer", "target": "Jessie", "type":"E"}
+]
+
+
+/*********** Simulation *************/
+var simulation = d3
+    .forceSimulation()
+    .nodes(nodes_data);
+
+var center_force = d3.forceCenter(width/2, height/2);
+var repel_force = d3.forceManyBody();
+
+var link_force = d3
+    .forceLink(links_data)
+    .id(function(d) { return d.name; });
+
+simulation
+  .force("center_force", center_force)
+  .force("repel_force", repel_force)
+  .force("links", link_force);
+
+simulation.on("tick", tick);
+
+
 /********** Node and Links ***********/
 var g = svg
     .append("g")
     .attr("class", "everything");
 
-var link = g.selectAll("line");
-
-var node = g.selectAll("circle");
-
-
-/************* Data **************/
-var root;
-
-function parse_json(error, json)
+function linkColor(d)
 {
-    if (error) throw error;
-    root = json;
-    update()
-}
-d3.json("../json/example.json", parse_json);
-
-function update()
-{
-    var nodes = flatten(root);
-    var links = d3
-        .layout()
-        .tree()
-        .links(nodes);
-
-    simulation.restart();
-
-    function link_id(d)
-    {
-        return d.target.id;
+    if (d.type == "A") {
+        return "green"
+    } else {
+        return "red"
     }
-    link = link.data(links, link_id)
-
-    link
-        .exit()
-        .remove();
-
-    link
-        .enter()
-        .append("line")
-        .attr("stroke-width", 2)
-        .style("stroke", linkColor)
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
-
-    
-    function node_id(d)
-    {
-        return node.id;
-    }
-    node = node.data(nodes, node_id);
-
-    node
-        .exit()
-        .remove()
-
-    node
-        .enter()
-        .append("circle")
-        .attr("r", 5)
-        
-
-
 }
 
+var link = g
+    .append("g")
+    .attr("class", "links")
+    .selectAll("line")
+    .data(links_data)
+    .enter()
+    .append("line")
+    .attr("stroke-width", 2)
+    .style("stroke", linkColor);
 
-const width = 1000,
-    height = 1000;
 
-let i = 0;
+function nodeColor(d)
+{
+    if (d.sex == "M") {
+        return "blue"
+    } else {
+        return "pink"
+    }
+}
 
-const root = d3.hierarchy(data);
-const transform = d3.zoomIdentity;
-let node, link;
-
-const svg = d3.select('#canvas').append('svg')
-    .call(d3.zoom().scaleExtent([1/2, 8]).on('zoom', zoomed))
-    .append('g')
-    .attr('transform', 'translate(40,0)');
+var node = g
+    .append("g")
+    .attr("class", "nodes")
+    .selectAll("circle")
+    .data(nodes_data)
+    .enter()
+    .append("circle")
+    .attr("r", 5)
+    .attr("fill", nodeColor);
 
 
 /********** Zoomable ************/
@@ -97,53 +128,52 @@ function zoom_actions()
     g.attr("transform", d3.event.transform)
 }
 
-function clicked(d) {
-    console.log(d.data.name);
+var zoom_handler = d3
+    .zoom()
+    .on("zoom", zoom_actions);
 
-    if (!d3.event.defaultPrevented) {
-    if (d.children) {
-        d._children = d.children;
-        d.children = null;
-    } else {
-        d.children = d._children;
-        d._children = null;
-    }
-    update()
-    }
-}
+zoom_handler(svg);
 
 
-function dragstarted(d) {
-    if (!d3.event.active) simulation.alphaTarget(0.3).restart()
+/********* Draggable *********/
+function drag_start(d)
+{
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
     d.fx = d.x
     d.fy = d.y
 }
 
-function dragged(d) {
-    d.fx = d3.event.x
-    d.fy = d3.event.y
+function drag_actions(d)
+{
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
 }
 
-function dragended(d) {
-    if (!d3.event.active) simulation.alphaTarget(0)
-    d.fx = null
-    d.fy = null
+function drag_end(d)
+{
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
 }
 
-function flatten(root) {
-    const nodes = []
-    function recurse(node) {
-    if (node.children) node.children.forEach(recurse)
-    if (!node.id) node.id = ++i;
-    else ++i;
-    nodes.push(node)
-    }
-    recurse(root)
-    return nodes
-}
+var drag_handler = d3.drag()
+    .on("start", drag_start)
+    .on("drag", drag_actions)
+    .on("end", drag_end);
 
-function zoomed() {
-    svg.attr('transform', d3.event.transform)
-}
+drag_handler(node);
 
-update() 
+
+/******** Tick *************/
+function tick() 
+{
+    link
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+  
+    node
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+}
