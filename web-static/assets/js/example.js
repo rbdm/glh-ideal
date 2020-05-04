8ef50482ff771a82b61d480594192ba60a5309d2
Josh Fourie <josh.fourie97@gmail.com>
2020-05-04 10:13:29 +1000

    populate getting started section
    Signed-off-by: Josh Fourie <josh.fourie97@gmail.com>

--- web-static/assets/js/example.js
+++ web-static/assets/js/example.js
@@ -1,179 +1,420 @@
-/********** Canvas *********/
-var svg = d3.select("#canvas");
-var width = svg.attr("width");
-var height = svg.attr("height");
+const data = {
+    "name": "flare",
+    "children": [
+        {
+        "name": "animate",
+        "children": [
+        {"name": "Easing", "size": 17010},
+        {"name": "FunctionSequence", "size": 5842},
+        {
+        "name": "interpolate",
+        "children": [
+            {"name": "ArrayInterpolator", "size": 1983},
+            {"name": "ColorInterpolator", "size": 2047},
+            {"name": "DateInterpolator", "size": 1375},
+            {"name": "Interpolator", "size": 8746},
+            {"name": "MatrixInterpolator", "size": 2202},
+            {"name": "NumberInterpolator", "size": 1382},
+            {"name": "ObjectInterpolator", "size": 1629},
+            {"name": "PointInterpolator", "size": 1675},
+            {"name": "RectangleInterpolator", "size": 2042}
+        ]
+        },
+        {"name": "ISchedulable", "size": 1041},
+        {"name": "Parallel", "size": 5176},
+        {"name": "Pause", "size": 449},
+        {"name": "Scheduler", "size": 5593},
+        {"name": "Sequence", "size": 5534},
+        {"name": "Transition", "size": 9201},
+        {"name": "Transitioner", "size": 19975},
+        {"name": "TransitionEvent", "size": 1116},
+        {"name": "Tween", "size": 6006}
+        ]
+        },
+        {
+        "name": "physics",
+        "children": [
+        {"name": "DragForce", "size": 1082},
+        {"name": "GravityForce", "size": 1336},
+        {"name": "IForce", "size": 319},
+        {"name": "NBodyForce", "size": 10498},
+        {"name": "Particle", "size": 2822},
+        {"name": "Simulation", "size": 9983},
+        {"name": "Spring", "size": 2213},
+        {"name": "SpringForce", "size": 1681}
+        ]
+        },
+        {
+        "name": "query",
+        "children": [
+        {"name": "AggregateExpression", "size": 1616},
+        {"name": "And", "size": 1027},
+        {"name": "Arithmetic", "size": 3891},
+        {"name": "Average", "size": 891},
+        {"name": "BinaryExpression", "size": 2893},
+        {"name": "Comparison", "size": 5103},
+        {"name": "CompositeExpression", "size": 3677},
+        {"name": "Count", "size": 781},
+        {"name": "DateUtil", "size": 4141},
+        {"name": "Distinct", "size": 933},
+        {"name": "Expression", "size": 5130},
+        {"name": "ExpressionIterator", "size": 3617},
+        {
+        "name": "methods",
+        "children": [
+            {"name": "add", "size": 593},
+            {"name": "and", "size": 330},
+            {"name": "average", "size": 287},
+            {"name": "count", "size": 277},
+            {"name": "distinct", "size": 292},
+            {"name": "div", "size": 595},
+            {"name": "eq", "size": 594},
+            {"name": "fn", "size": 460},
+            {"name": "gt", "size": 603},
+            {"name": "gte", "size": 625},
+            {"name": "iff", "size": 748},
+            {"name": "isa", "size": 461},
+            {"name": "lt", "size": 597},
+            {"name": "lte", "size": 619},
+            {"name": "max", "size": 283},
+            {"name": "min", "size": 283},
+            {"name": "mod", "size": 591},
+            {"name": "mul", "size": 603},
+            {"name": "neq", "size": 599},
+            {"name": "not", "size": 386},
+            {"name": "or", "size": 323},
+            {"name": "orderby", "size": 307},
+            {"name": "range", "size": 772},
+            {"name": "select", "size": 296},
+            {"name": "stddev", "size": 363},
+            {"name": "sub", "size": 600},
+            {"name": "sum", "size": 280},
+            {"name": "update", "size": 307},
+            {"name": "variance", "size": 335},
+            {"name": "where", "size": 299},
+            {"name": "xor", "size": 354},
+            {"name": "_", "size": 264}
+        ]
+        },
+        {"name": "Minimum", "size": 843},
+        {"name": "Not", "size": 1554},
+        {"name": "Or", "size": 970},
+        {"name": "Query", "size": 13896},
+        {"name": "Range", "size": 1594},
+        {"name": "StringUtil", "size": 4130},
+        {"name": "Sum", "size": 791},
+        {"name": "Variable", "size": 1124},
+        {"name": "Variance", "size": 1876},
+        {"name": "Xor", "size": 1101}
+        ]
+        },
+        {
+        "name": "scale",
+        "children": [
+        {"name": "IScaleMap", "size": 2105},
+        {"name": "LinearScale", "size": 1316},
+        {"name": "LogScale", "size": 3151},
+        {"name": "OrdinalScale", "size": 3770},
+        {"name": "QuantileScale", "size": 2435},
+        {"name": "QuantitativeScale", "size": 4839},
+        {"name": "RootScale", "size": 1756},
+        {"name": "Scale", "size": 4268},
+        {"name": "ScaleType", "size": 1821},
+        {"name": "TimeScale", "size": 5833}
+        ]
+        },
+        {
+        "name": "vis",
+        "children": [
+        {
+        "name": "axis",
+        "children": [
+            {"name": "Axes", "size": 1302},
+            {"name": "Axis", "size": 24593},
+            {"name": "AxisGridLine", "size": 652},
+            {"name": "AxisLabel", "size": 636},
+            {"name": "CartesianAxes", "size": 6703}
+        ]
+        },
+        {
+        "name": "controls",
+        "children": [
+            {"name": "AnchorControl", "size": 2138},
+            {"name": "ClickControl", "size": 3824},
+            {"name": "Control", "size": 1353},
+            {"name": "ControlList", "size": 4665},
+            {"name": "DragControl", "size": 2649},
+            {"name": "ExpandControl", "size": 2832},
+            {"name": "HoverControl", "size": 4896},
+            {"name": "IControl", "size": 763},
+            {"name": "PanZoomControl", "size": 5222},
+            {"name": "SelectionControl", "size": 7862},
+            {"name": "TooltipControl", "size": 8435}
+        ]
+        },
+        {
+        "name": "data",
+        "children": [
+            {"name": "Data", "size": 20544},
+            {"name": "DataList", "size": 19788},
+            {"name": "DataSprite", "size": 10349},
+            {"name": "EdgeSprite", "size": 3301},
+            {"name": "NodeSprite", "size": 19382},
+            {
+            "name": "render",
+            "children": [
+            {"name": "ArrowType", "size": 698},
+            {"name": "EdgeRenderer", "size": 5569},
+            {"name": "IRenderer", "size": 353},
+            {"name": "ShapeRenderer", "size": 2247}
+            ]
+            },
+            {"name": "ScaleBinding", "size": 11275},
+            {"name": "Tree", "size": 7147},
+            {"name": "TreeBuilder", "size": 9930}
+        ]
+        },
+        {
+        "name": "events",
+        "children": [
+            {"name": "DataEvent", "size": 2313},
+            {"name": "SelectionEvent", "size": 1880},
+            {"name": "TooltipEvent", "size": 1701},
+            {"name": "VisualizationEvent", "size": 1117}
+        ]
+        },
+        {
+        "name": "legend",
+        "children": [
+            {"name": "Legend", "size": 20859},
+            {"name": "LegendItem", "size": 4614},
+            {"name": "LegendRange", "size": 10530}
+        ]
+        },
+        {
+        "name": "operator",
+        "children": [
+            {
+            "name": "distortion",
+            "children": [
+            {"name": "BifocalDistortion", "size": 4461},
+            {"name": "Distortion", "size": 6314},
+            {"name": "FisheyeDistortion", "size": 3444}
+            ]
+            },
+            {
+            "name": "encoder",
+            "children": [
+            {"name": "ColorEncoder", "size": 3179},
+            {"name": "Encoder", "size": 4060},
+            {"name": "PropertyEncoder", "size": 4138},
+            {"name": "ShapeEncoder", "size": 1690},
+            {"name": "SizeEncoder", "size": 1830}
+            ]
+            },
+            {
+            "name": "filter",
+            "children": [
+            {"name": "FisheyeTreeFilter", "size": 5219},
+            {"name": "GraphDistanceFilter", "size": 3165},
+            {"name": "VisibilityFilter", "size": 3509}
+            ]
+            },
+            {"name": "IOperator", "size": 1286},
+            {
+            "name": "label",
+            "children": [
+            {"name": "Labeler", "size": 9956},
+            {"name": "RadialLabeler", "size": 3899},
+            {"name": "StackedAreaLabeler", "size": 3202}
+            ]
+            },
+            {
+            "name": "layout",
+            "children": [
+            {"name": "AxisLayout", "size": 6725},
+            {"name": "BundledEdgeRouter", "size": 3727},
+            {"name": "CircleLayout", "size": 9317},
+            {"name": "CirclePackingLayout", "size": 12003},
+            {"name": "DendrogramLayout", "size": 4853},
+            {"name": "ForceDirectedLayout", "size": 8411},
+            {"name": "IcicleTreeLayout", "size": 4864},
+            {"name": "IndentedTreeLayout", "size": 3174},
+            {"name": "Layout", "size": 7881},
+            {"name": "NodeLinkTreeLayout", "size": 12870},
+            {"name": "PieLayout", "size": 2728},
+            {"name": "RadialTreeLayout", "size": 12348},
+            {"name": "RandomLayout", "size": 870},
+            {"name": "StackedAreaLayout", "size": 9121},
+            {"name": "TreeMapLayout", "size": 9191}
+            ]
+            },
+            {"name": "Operator", "size": 2490},
+            {"name": "OperatorList", "size": 5248},
+            {"name": "OperatorSequence", "size": 4190},
+            {"name": "OperatorSwitch", "size": 2581},
+            {"name": "SortOperator", "size": 2023}
+        ]
+        },
+        {"name": "Visualization", "size": 16540}
+        ]
+        }
+    ]
+}
 
+const width = 1000;
+const height = 1000;
 
-/************* Data **************/
-//Characters
-var nodes_data =  [
-    {"name": "Lillian", "sex": "F"},
-    {"name": "Gordon", "sex": "M"},
-    {"name": "Sylvester", "sex": "M"},
-    {"name": "Mary", "sex": "F"},
-    {"name": "Helen", "sex": "F"},
-    {"name": "Jamie", "sex": "M"},
-    {"name": "Jessie", "sex": "F"},
-    {"name": "Ashton", "sex": "M"},
-    {"name": "Duncan", "sex": "M"},
-    {"name": "Evette", "sex": "F"},
-    {"name": "Mauer", "sex": "M"},
-    {"name": "Fray", "sex": "F"},
-    {"name": "Duke", "sex": "M"},
-    {"name": "Baron", "sex": "M"},
-    {"name": "Infante", "sex": "M"},
-    {"name": "Percy", "sex": "M"},
-    {"name": "Cynthia", "sex": "F"}
-]
+var i = 0;
 
-//Relationships
-//type: A for Ally, E for Enemy
-var links_data = [
-    {"source": "Sylvester", "target": "Gordon", "type":"A" },
-    {"source": "Sylvester", "target": "Lillian", "type":"A" },
-    {"source": "Sylvester", "target": "Mary", "type":"A"},
-    {"source": "Sylvester", "target": "Jamie", "type":"A"},
-    {"source": "Sylvester", "target": "Jessie", "type":"A"},
-    {"source": "Sylvester", "target": "Helen", "type":"A"},
-    {"source": "Helen", "target": "Gordon", "type":"A"},
-    {"source": "Mary", "target": "Lillian", "type":"A"},
-    {"source": "Ashton", "target": "Mary", "type":"A"},
-    {"source": "Duncan", "target": "Jamie", "type":"A"},
-    {"source": "Gordon", "target": "Jessie", "type":"A"},
-    {"source": "Sylvester", "target": "Fray", "type":"E"},
-    {"source": "Fray", "target": "Mauer", "type":"A"},
-    {"source": "Fray", "target": "Cynthia", "type":"A"},
-    {"source": "Fray", "target": "Percy", "type":"A"},
-    {"source": "Percy", "target": "Cynthia", "type":"A"},
-    {"source": "Infante", "target": "Duke", "type":"A"},
-    {"source": "Duke", "target": "Gordon", "type":"A"},
-    {"source": "Duke", "target": "Sylvester", "type":"A"},
-    {"source": "Baron", "target": "Duke", "type":"A"},
-    {"source": "Baron", "target": "Sylvester", "type":"E"},
-    {"source": "Evette", "target": "Sylvester", "type":"E"},
-    {"source": "Cynthia", "target": "Sylvester", "type":"E"},
-    {"source": "Cynthia", "target": "Jamie", "type":"E"},
-    {"source": "Mauer", "target": "Jessie", "type":"E"}
-]
+const root = d3.hierarchy(data);
+const transform = d3.zoomIdentity;
+let node, link;
 
+const svg = d3
+    .select('#canvas')
+    .append('svg')
+    .call(d3.zoom().scaleExtent([1/2, 8]).on('zoom', zoomed))
+    .append('g')
+    .attr('transform', 'translate(40,0)');
 
-/*********** Simulation *************/
-var simulation = d3
+const simulation = d3
     .forceSimulation()
-    .nodes(nodes_data);
+    .force('link', d3.forceLink().id(function(d) { return d.id; }))
+    .force('charge', d3.forceManyBody().strength(-15).distanceMax(300))
+    .force('center', d3.forceCenter( width/2, height/4 ))
+    .on('tick', ticked)
 
-var center_force = d3.forceCenter(width/2, height/2);
-var repel_force = d3.forceManyBody();
-
-var link_force = d3
-    .forceLink(links_data)
-    .id(function(d) { return d.name; });
-
-simulation
-  .force("center_force", center_force)
-  .force("repel_force", repel_force)
-  .force("links", link_force);
-
-simulation.on("tick", tick);
-
-
-/********** Node and Links ***********/
-var g = svg
-    .append("g")
-    .attr("class", "everything");
-
-function linkColor(d)
+function update() 
 {
-    if (d.type == "A") {
-        return "green"
-    } else {
-        return "red"
+    const nodes = flatten(root)
+    const links = root.links()
+    
+    link = svg
+        .selectAll('.link')
+        .data(links, function(d){ return d.target.id })
+    
+    link.exit().remove()
+    
+    const linkEnter = link
+        .enter()
+        .append('line')
+        .attr('class', 'link')
+        .style('stroke', '#000' )
+        .style('opacity', '0.2')
+        .style('stroke-width', 2)
+    
+    link = linkEnter.merge(link)
+    
+    node = svg
+        .selectAll('.node')
+        .data(nodes, function(d){ return d.id })
+
+    node.exit().remove()
+    
+    const nodeEnter = node
+        .enter()
+        .append('g')
+        .attr('class', 'node')
+        .attr('stroke', '#666')
+        .attr('stroke-width', 2)
+        .style('fill', color)
+        .style('opacity', 1)
+        .on('click', clicked)
+        .call(d3.drag()
+            .on('start', dragstarted)
+            .on('drag', dragged)
+            .on('end', dragended))
+    
+    nodeEnter.append('circle')
+        .attr("r", function(d) { return Math.sqrt(d.data.size) / 10 || 4.5; })
+        .style('text-anchor', function(d){ return d.children ? 'end' : 'start'; })
+        .text(function(d){ return d.data.name })
+
+    node = nodeEnter.merge(node)
+    simulation.nodes(nodes)
+    simulation.force('link').links(links)
+}
+
+function sizeContain(num) {
+    num = num > 1000 ? num/1000 : num/100
+    if (num < 4) num = 4
+    return num
+}
+
+function color(d) {
+    return d._children ? "#51A1DC" // collapsed package
+        : d.children ? "#51A1DC" // expanded package
+        : "#F94B4C"; // leaf node
+}
+
+function radius(d) {
+    return d._children ? 8
+    : d.children ? 8
+    : 4
+}
+
+function ticked() {
+    link
+        .attr('x1', function(d){ return d.source.x; })
+        .attr('y1', function(d){ return d.source.y; })
+        .attr('x2', function(d){ return d.target.x; })
+        .attr('y2', function(d){ return d.target.y; })
+    
+    node
+        .attr('transform', function(d){ return `translate(${d.x}, ${d.y})`})
+}
+
+function clicked(d) {
+    if (!d3.event.defaultPrevented) {
+        if (d.children) {
+            d._children = d.children;
+            d.children = null;
+        } else {
+            d.children = d._children;
+            d._children = null;
+        }
+        update()
     }
 }
 
-var link = g
-    .append("g")
-    .attr("class", "links")
-    .selectAll("line")
-    .data(links_data)
-    .enter()
-    .append("line")
-    .attr("stroke-width", 2)
-    .style("stroke", linkColor);
 
-
-function nodeColor(d)
+function dragstarted(d) 
 {
-    if (d.sex == "M") {
-        return "blue"
-    } else {
-        return "pink"
-    }
-}
-
-var node = g
-    .append("g")
-    .attr("class", "nodes")
-    .selectAll("circle")
-    .data(nodes_data)
-    .enter()
-    .append("circle")
-    .attr("r", 5)
-    .attr("fill", nodeColor);
-
-
-/********** Zoomable ************/
-function zoom_actions()
-{
-    g.attr("transform", d3.event.transform)
-}
-
-var zoom_handler = d3
-    .zoom()
-    .on("zoom", zoom_actions);
-
-zoom_handler(svg);
-
-
-/********* Draggable *********/
-function drag_start(d)
-{
-    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart()
     d.fx = d.x
     d.fy = d.y
 }
 
-function drag_actions(d)
+function dragged(d) 
 {
-    d.fx = d3.event.x;
-    d.fy = d3.event.y;
+    d.fx = d3.event.x
+    d.fy = d3.event.y
 }
 
-function drag_end(d)
+function dragended(d) 
 {
-    if (!d3.event.active) simulation.alphaTarget(0);
-    d.fx = null;
-    d.fy = null;
+    if (!d3.event.active) simulation.alphaTarget(0)
+    d.fx = null
+    d.fy = null
 }
 
-var drag_handler = d3.drag()
-    .on("start", drag_start)
-    .on("drag", drag_actions)
-    .on("end", drag_end);
-
-drag_handler(node);
-
-
-/******** Tick *************/
-function tick() 
+function flatten(root) 
 {
-    link
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
-  
-    node
-        .attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
+    const nodes = []
+    function recurse(node) {
+    if (node.children) node.children.forEach(recurse)
+    if (!node.id) node.id = ++i;
+    else ++i;
+    nodes.push(node)
+    }
+    recurse(root)
+    return nodes
 }
+
+function zoomed() 
+{
+    svg.attr('transform', d3.event.transform)
+}
+
+update() 
